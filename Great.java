aa88a6d94efb05829a34488d328909553cae3b92
SABARINATH6691 <53590475+SABARINATH6691@users.noreply.github.com>
2019-08-14 08:08:06 +0530

    Create Great.java

---
+++ Great.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+class Largest
+{
+  public static void main(String args[])
+  {
+    int x, y, z;
+    System.out.println("Enter three integers");
+    Scanner in = new Scanner(System.in);
+ 
+    x = in.nextInt();
+    y = in.nextInt();
+    z = in.nextInt();
+ 
+    if (x > y && x > z)
+      System.out.println("The largest number is "+x);
+    else if (y > z)
+      System.out.println("The largest number is "+y);
+    
+    else
+      System.out.println("The largest number is "+z);
+  }
+}
