6a3704a0999d0c4e56f622f6eb4afafbed0e7c18
SABARINATH6691 <53590475+SABARINATH6691@users.noreply.github.com>
2019-08-14 08:28:10 +0530

    Create Prnt.java

---
+++ Prnt.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+class Largest
+{
+  public static void main(String args[])
+  {
+    int N;
+    System.out.println("Enter the integers");
+    Scanner in = new Scanner(System.in);
+ 
+    N = in.nextInt();
+    for(int i=1;i<=N;i++)
+    {
+    	System.out.println("Hello World");
+    }
+    
+    }
+}
