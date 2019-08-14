6ef041c7081d00a3362bbbcc2c10c031970f3d77
SABARINATH6691 <53590475+SABARINATH6691@users.noreply.github.com>
2019-08-14 06:03:07 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -8,18 +8,17 @@ import java.util.*;
  	   char n;
 		System.out.println("enter the number");
 		Scanner Input=new Scanner(System.in);
-		n=Input.next().charAt(0);
-		if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+		 n=Input.next().charAt(0);
+		if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
 		{
 			System.out.println("the number is vowel ");
 		}
-		else
-		{
-			System.out.println("the number is consonant");
-		}
+		else {
+	   if((n>='a'&&n<='z')||(n>='A'&&n<='Z'))
+		System.out.println(n+" is a Consonant");
+	   else
+		System.out.println("Input is invalid");		
+        }
 		}
  }
-    
-	
-
     
