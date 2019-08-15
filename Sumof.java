a7874807be43cd8814b079229e91a0bb8de4c27a
SABARINATH6691 <53590475+SABARINATH6691@users.noreply.github.com>
2019-08-15 17:40:06 +0530

    Update Sumof.java

--- Sumof.java
+++ Sumof.java
@@ -5,11 +5,11 @@ public class Array
     {
         int N,k,sum = 0;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter no. of elements you want in array & k value\n");
+        System.out.print("Enter no. of elements you want in array & k value");
         N= s.nextInt();
         k= s.nextInt();
         int a[] = new int[N];
-        System.out.println("Enter all the elements:\n ");
+        System.out.println("Enter all the elements: ");
         for(int i = 0; i < N; i++)
         {
             a[i] = s.nextInt();
